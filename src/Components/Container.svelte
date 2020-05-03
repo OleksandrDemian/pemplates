0e5a9cca1cf20ebf2917601d86f9cbf5906b931a
Oleksandr Demian <oleksandr@addonsforgapps.com>
2020-05-03 08:49:05 +0200

    UI

--- src/Components/Container.svelte
+++ src/Components/Container.svelte
@@ -7,8 +7,9 @@
 		padding: 20px;
 		min-width: 300px;
 		margin: 8px auto;
-		box-shadow: 0 0 4px rgb(35, 35, 35);
-		/*border: 1px solid var(--secondary);*/
+		/*box-shadow: 4px 4px var(--secondary);*/
+		border: 1px solid var(--secondary);
+		border-radius: 2px;
 		background-color: var(--surface);
 	}
 </style>
