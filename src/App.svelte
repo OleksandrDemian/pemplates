2d8ff8c3e94a49fa25961ebda9667acd207e6607
Oleksandr Demian <oleksandr@addonsforgapps.com>
2020-05-03 11:15:33 +0200

    UI Update

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 	import {onMount, onDestroy} from "svelte";
 	import {addListener, removeListener} from "./utils/dropListener";
 	import Settings from "./UI/Views/Settings.svelte";
+	import Container from "./UI/Components/Container.svelte";
 
 	let activeTab = "search";
 	let name = null;
@@ -29,12 +30,12 @@
 </script>
 
 <div id="navigation">
-	<h1 style="padding-left: 10px">Menu</h1>
-	<button on:click={() => navigate("projects")}>Projects</button><br/>
-	<button on:click={() => navigate("templates")}>Templates</button><br/>
-	<button on:click={() => navigate("search")}>Search</button><br/>
-	<button on:click={() => navigate("new")}>New</button><br/>
-	<button on:click={() => navigate("settings")}>Settings</button><br/>
+	<h1>Menu</h1>
+	<h3 class="cursor" on:click={() => navigate("projects")}><u>Projects</u></h3>
+	<h3 class="cursor" on:click={() => navigate("templates")}><u>Templates</u></h3>
+	<h3 class="cursor" on:click={() => navigate("search")}><u>Search</u></h3>
+	<h3 class="cursor" on:click={() => navigate("new")}><u>New</u></h3>
+	<h3 class="cursor" on:click={() => navigate("settings")}><u>Settings</u></h3>
 </div>
 
 <div id="content">
@@ -57,18 +58,12 @@
 </div>
 
 <style>
-
 	#navigation {
 		display: inline-block;
 		vertical-align: top;
-		padding-top: 10px;
+		padding: 10px;
 		width: 25%;
 		min-width: 250px;
-	}
-
-	button {
-		width: 50%;
-		border-radius: unset;
 	}
 
 	#content {
