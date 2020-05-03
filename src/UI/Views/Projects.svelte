05b33076e1f6370aacb6bae3f19642298e946df9
Oleksandr Demian <oleksandr@addonsforgapps.com>
2020-05-03 16:17:01 +0200

    Filter templates/projects

--- src/UI/Views/Projects.svelte
+++ src/UI/Views/Projects.svelte
@@ -3,17 +3,43 @@
 	import Project from "../Sections/Project/Project.svelte";
 
 	let arr = [];
+	let filterValue = "";
 
 	$: {
 		arr = [...$projectsRepo];
 		arr.reverse();
+
+		//todo: same as Templates
+		if(filterValue.length > 0){
+			let lower = filterValue.toLowerCase();
+			arr = arr.filter(project => {
+				if(project.name.toLowerCase().includes(lower)){
+					return true;
+                } else if(project.description){
+					return project.description.toLowerCase().includes(lower);
+                }
+
+				return false;
+            });
+        }
     }
 </script>
 
 <div id="projectsList">
+    <input bind:value={filterValue} placeholder="Filter projects" />
     {#each arr as project}
         <Project project={project} />
     {:else}
-        <h3>There are no projects</h3>
+        { #if filterValue.length > 0 }
+            <h3>There is no match with {filterValue}</h3>
+        {:else}
+			<h3>There are no projects</h3>
+        {/if}
     {/each}
 </div>
+
+<style>
+    input {
+        width: 100%;
+    }
+</style>
--- src/UI/Views/Templates.svelte
+++ src/UI/Views/Templates.svelte
@@ -3,22 +3,46 @@
 	import Template from "../Sections/Templates/Template.svelte";
 
 	let arr = [];
-
+	let filterValue = "";
 	$: {
 		arr = [...$templates];
 		arr.reverse();
+
+		//todo: same as Projects
+		if(filterValue.length > 0){
+			let lower = filterValue.toLowerCase();
+			arr = arr.filter(template => {
+				if(template.name.toLowerCase().includes(lower)){
+					return true;
+				} else if(template.description){
+					return template.description.toLowerCase().includes(lower);
+				}
+
+				return false;
+			});
+		}
 	}
 </script>
 
 <div id="templatesList">
+	<input bind:value={filterValue} placeholder="Filter templates" />
 	{#each arr as template}
 		<Template template={template} />
 	{:else}
-		<h3>There are no templates</h3>
-		<ul>
-			<li>Drop folder here to create a local template</li>
-			<li>Search for templates in "Search" section</li>
-			<li>Create new template in "New template" section</li>
-		</ul>
+		{ #if filterValue.length > 0 }
+			<h3>There is no match with {filterValue}</h3>
+		{:else}
+			<ul>
+				<li>Drop folder here to create a local template</li>
+				<li>Search for templates in "Search" section</li>
+				<li>Create new template in "New template" section</li>
+			</ul>
+		{/if}
 	{/each}
 </div>
+
+<style>
+	input {
+		width: 100%;
+	}
+</style>
