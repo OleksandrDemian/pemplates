c679ee3c54fbb44ef4fa09b60de90e51ae082700
Oleksandr Demian <oleksandr@addonsforgapps.com>
2020-05-05 19:51:18 +0200

    Select topic

---
+++ src/UI/Components/SearchTopicSelect.svelte
@@ -0,0 +1,18 @@
+<script>
+	import {onMount} from "svelte";
+
+	export let topicValue = null;
+
+	onMount(() => {
+		if(topicValue == null){
+			topicValue = "template";
+		}
+	});
+</script>
+
+<select bind:value={topicValue}>
+	<option value="template">Templates</option>
+	<option value="boilerplate">Boilerplate</option>
+	<option value="starter-kit">Starter-kit</option>
+<!--	<option value="any">Anywhere</option>-->
+</select>
--- src/UI/Views/Search.svelte
+++ src/UI/Views/Search.svelte
@@ -3,12 +3,14 @@
 	import {notify} from "power-notifier";
 	import Loading from "../Components/Loading.svelte";
 	import {createEventDispatcher} from "svelte";
+	import SearchTopicSelect from "../Components/SearchTopicSelect.svelte";
 
 	/*
 	use search value to bind input and filter to show in case of null result
 	 */
 	let search_value = null;
 	let filter_value = null;
+	let selectedTopic = null;
 	let result = null;
 	let dispatcher = createEventDispatcher();
 
@@ -16,7 +18,7 @@
 
 	const onKeyUp = (e) => {
 		//enter -> 13
-		if(e.keyCode === 13){
+		if (e.keyCode === 13) {
 			search();
 		}
 	};
@@ -26,7 +28,8 @@
 		isSearching = true;
 		filter_value = search_value;
 		try {
-			const url = `https://api.github.com/search/repositories?q=${search_value}+topic:template&sort=stars`;
+			// const url = `https://api.github.com/search/repositories?q=${search_value}+topic:template&sort=stars`;
+			const url = `https://api.github.com/search/repositories?q=${search_value}+topic:${selectedTopic}&sort=stars`;
 			const response = (await fetch(url));
 			const data = await response.json();
 			result = data.items;
@@ -44,6 +47,7 @@
 </script>
 
 <input type="text" placeholder="Search for templates on GitHub" bind:value={search_value} on:keyup={onKeyUp} />
+<SearchTopicSelect bind:topicValue={selectedTopic} />
 <button on:click={search}>Search</button>
 
 <div id="searchResult">
